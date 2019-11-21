0c6a41e77e532f64706bdbd7fd5675cc1c867332
zhijiehua <2010225130@qq.com>
2019-11-21 11:10:22 +0800

    test

---
+++ vueJS/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    1111
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      a: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
