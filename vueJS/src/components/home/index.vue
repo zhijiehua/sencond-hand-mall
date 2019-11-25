913e27f58c8abdd3f2fe895f6238160c4cb89436
zhijiehua <2010225130@qq.com>
2019-11-25 09:51:29 +0800

    路由设置

---
+++ vueJS/src/components/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <menu-index></menu-index>
+  </div>
+</template>
+
+<script>
+import menuIndex from './menu/index'
+export default {
+  components: {
+    menuIndex
+  },
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
