eb6f56050d5817973880186d7d88251dc0e72523
zhijiehua <2010225130@qq.com>
2019-11-27 23:12:44 +0800

    增加选择地址的抽屉

--- vueJS/src/components/home/menu/index.vue
+++ vueJS/src/components/home/menu/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 头部导航主题 -->
     <el-menu
     :default-active="activeIndex2"
     class="el-menu-demo"
@@ -8,7 +9,9 @@
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b">
-    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-menu-item index="1">
+      <i class="loaction el-icon-location-outline" @click="openDraw">北京</i>
+    </el-menu-item>
     <el-submenu index="2">
         <template slot="title">我的工作台</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
@@ -24,13 +27,26 @@
     <el-menu-item index="3" disabled>消息中心</el-menu-item>
     <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
     </el-menu>
+    <!-- 抽屉 -->
+    <el-drawer
+      title="请选择您所在的位置"
+      :visible.sync="drawerDialog"
+      direction="ltr"
+      :with-header="false">
+      <location-choose></location-choose>
+    </el-drawer>
   </div>
 </template>
 
 <script>
+import locationChoose from './locationChoose'
 export default {
+  components: {
+    locationChoose
+  },
   data () {
     return {
+      drawerDialog: false,
       activeIndex: '1',
       activeIndex2: '1'
     }
@@ -38,11 +54,16 @@ export default {
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
+    },
+    openDraw () { // 打开选择地址的抽屉
+      this.drawerDialog = true
     }
   }
 }
 </script>
 
 <style scoped>
-
+.loaction {
+  font-size: 14px;
+}
 </style>
---
+++ vueJS/src/components/home/menu/locationChoose.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="choose-location">
+        请选择城市
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
