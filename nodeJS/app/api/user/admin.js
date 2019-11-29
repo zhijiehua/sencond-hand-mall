837d99ed8482789ce0dc5ebbcd5e8d33915882b4
zhijiehua <2010225130@qq.com>
2019-11-29 10:40:53 +0800

    测试

--- nodeJS/app/api/user/admin.js
+++ nodeJS/app/api/user/admin.js
@@ -5,20 +5,12 @@ const {
   RegisterValidator,
   AdminLoginValidator
 } = require('../validators/user/admin')
-// const {LoginManager} = require('../../service/login');
+
 const router = new Router({
   prefix: '/user/admin'
 })
 
 router.post('/auth',  async (ctx) => {
-  console.log('/////////////////// ctx //////////////////////')
-  console.log(ctx)
-  const v = await new AdminLoginValidator().validate(ctx);
-  console.log('/////////////////// v //////////////////////')
-  console.log(v)
-  console.log(v.get('body.email'))
-  console.log(v.get('body.password'))
-  // let token = await LoginManager.adminLogin(v.get('body.email'), v.get('body.password'));
   ctx.response.status = 200;
   ctx.body = res.json('444')
 })
