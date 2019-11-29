543a367d5385e89200e42c67a705f4c9ee9a71aa
zhijiehua <2010225130@qq.com>
2019-11-29 10:46:10 +0800

    前端

--- vueJS/src/components/home/menu/locationChoose.vue
+++ vueJS/src/components/home/menu/locationChoose.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="choose-location">
-        请选择城市
+        <!-- 根据输入地址搜索 -->
+        <el-input v-model="inputCity" placeholder="输入中文/拼音/首字母">
+            <i
+                class="el-icon-search el-input__icon"
+                slot="suffix">
+            </i>
+        </el-input>
+        <div>当前/历史</div>
+
+        <div>热门城市</div>
+        <div></div>
     </div>
 </template>
 
@@ -8,7 +18,7 @@
 export default {
   data () {
     return {
-
+      inputCity: ''
     }
   }
 }
